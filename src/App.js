b721edba1a14705b639577522fba3197c26f76db
Nilufar <example@mail.ru>
2019-05-11 14:38:22 +0500

    searchkit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { 
+  SearchBox, 
+  SearchkitComponent,
+  Hits,
+  HitItem,
+  Pagination,
+  RefinementListFilter,
+  Layout,
+  TopBar,
+  LayoutResults,
+  LayoutBody,
+  SideBar,
+  RangeFilter,
+  SelectedFilters,
+  CheckboxFilter,
+  RangeQuery,
+  TermQuery
+} from 'searchkit'
+import {get} from 'lodash'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends SearchkitComponent {
+  render() {
+
+    const HitItem = (props) => {
+      let url =  "http://www.imdb.com/title/" + props.result._source.imdbId
+
+     return ( <div className={props.bemBlocks.item().mix(props.bemBlocks.container("item"))}>
+                <a href={url}>
+                  <img data-qa="poster" className={props.bemBlocks.item("poster")} src={props.result._source.poster} width="170" height="240"/>
+                  <div data-qa="title" className={props.bemBlocks.item("title")} dangerouslySetInnerHTML={{__html: get(props.result,"highlight.title",props.result._source.title)}}></div>
+                </a>
+              </div>  
+      )
+    }
+
+    const RefinementOption = (props) => (
+      <div className={props.bemBlocks.option().state({selected:props.selected}).mix(props.bemBlocks.container("item"))} onClick={props.onClick}>
+        <div className={props.bemBlocks.option("text")}>{props.label}</div>
+        <div className={props.bemBlocks.option("count")}>{props.count}</div>
+      </div>
+    )
+
+    const SelectedFilter = (props) => (
+      <div className={props.bemBlocks.option()
+        .mix(props.bemBlocks.container("item"))
+        .mix(`selected-filter--${props.filterId}`)()}>
+        <div className={props.bemBlocks.option("name")}>{props.labelKey}: {props.labelValue}</div>
+        <div className={props.bemBlocks.option("remove-action")} onClick={props.removeFilter}>x</div>
+      </div>
+    )
+       
+    return (
+      <div>
+        <Layout>  
+          <TopBar>
+           <SearchBox 
+            searchOnChange={true}
+            querOptions={{analyzer: "standard"}}
+            queryFields={["title^5", "languages", "text"]}
+            />
+          </TopBar>
+          <LayoutBody>
+            <SideBar>
+             <RefinementListFilter 
+                field="languages.raw"
+                title="Lenguages"
+                id="lenguages" itemComponent={RefinementOption}
+              />
+              <RangeFilter field="metaScore" id="metascore" min={0} max={100} showHistogram={false} title="MetaScore"/>
+              
+              <CheckboxFilter id="old-moves" title="Movile filter" label="Old movies" filter={
+                Boolean([
+                  RangeQuery("year", {lt: 1970}),
+                  TermQuery("type.raw", "Movie")
+                ])
+              } />
+              <CheckboxFilter id="recent" title="Date" label="Recent" filter={RangeQuery("year", {gt: 2012})}/>
+            </SideBar>
+            <LayoutResults>
+              <SelectedFilters/>
+              <Hits 
+              mod="sk-hits-grid"
+              hitsPerPage={50} 
+              highlightFields={["title"]} 
+              sourceFilter={["title", "poster", "imdbId"]}
+              itemComponent={HitItem}
+              />
+              <Pagination showNumbers={true}/>
+            </LayoutResults>
+          </LayoutBody>
+        </Layout>
+       
+       
+       
+      </div>
+    )
+  }
+  
 }
 
-export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import App from './App'
+import {
+    SearchkitManager,
+    SearchkitProvider
+} from 'searchkit'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const sk = new SearchkitManager("http://demo.searchkit.co/api/movies/", {
+})
+console.log("move api",sk)
+
+ReactDOM.render(
+    <SearchkitProvider searchkit={sk}>
+        <App/>
+    </SearchkitProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
